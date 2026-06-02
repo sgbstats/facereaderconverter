2fd4454dbc339d54ecd2d1649e86ab4e8f94e3d0
sgbstats <sebastiangbate@gmail.com>
2026-06-02 11:34:04 +0100

    starting work

---
+++ R/synchrony.R
@@ -0,0 +1,4 @@
+#' Takes converted data and calculates synchrony
+#'
+#' @param coding_df a dataframe or otherwise from a FaceReader output. id and subject should be present.
+#' @param denominator a dataframe of of converted data
